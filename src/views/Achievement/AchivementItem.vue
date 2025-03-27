7fed956efb90fbac83b5573e590d75193d5a0348
hw1020 <wangchuanhong1020@outlook.com>
2025-03-27 15:21:44 +0800

    Update AchivementItem.vue

--- src/views/Achievement/AchivementItem.vue
+++ src/views/Achievement/AchivementItem.vue
@@ -129,7 +129,7 @@ export default defineComponent({
             try {
                 gameNameRR = navigator.language.toLowerCase() === 'zh-cn' ? '原神' : 'Genshin·Impact'
             } catch (e) {}
-            gameNameRR = gameNameRR + ' ' + i18n.amos[i.name] + ' ' + '成就'
+            gameNameRR += ' ' + i18n.amos[i.name] + ' '
             return `https://www.bing.com/search?q=${encodeURIComponent(gameNameRR)}`
         }
         return {
